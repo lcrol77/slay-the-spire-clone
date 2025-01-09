1c9e2d3a055ab02e3a2da14af0a21f97824cbb8c
Logan Croley <logan.croley.18@gmail.com>
2025-01-09 16:13:27 -0500

    fix: corrected bug where block was not resetting

--- Scenes/Battle/battle.gd
+++ Scenes/Battle/battle.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var char_stats: CharacterStats
 @onready var battle_ui: BattleUI = $BattleUI
 @onready var player_handler: PlayerHandler = $PlayerHandler
+@onready var player: Player = $Player
 
 func _ready() -> void:
 	# Normally, we would do this on a "run"
@@ -11,6 +12,7 @@ func _ready() -> void:
 	# between battles
 	var new_stats: CharacterStats = char_stats.create_instance()
 	battle_ui.char_stats = new_stats
+	player.stats = new_stats
 	
 	Events.player_turn_ended.connect(player_handler.end_turn)
 	Events.player_hand_discarded.connect(player_handler.start_turn)
